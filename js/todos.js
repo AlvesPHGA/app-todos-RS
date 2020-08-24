ad593e4f068db43743a16357eba46b1a6cdac428
Pedro.Alves <pedrohenriquegomesalves@gmail.com>
2020-08-24 10:52:19 -0300

    Iniciando a aplicaçao

---
+++ js/todos.js
@@ -0,0 +1,9 @@
+var listTodos= document.querySelector('.list ul')
+var form_list= document.querySelector('.form-list input')
+var button_todos= document.querySelector('.form_list button')
+
+var todos= [
+    "Fazer café",
+    "Ligar para o pai",
+    "Fazer lista de compras"
+]
