2b6403a358d0ebd71f06b12e6b4085ab6e291b8c
Pedro.Alves <pedrohenriquegomesalves@gmail.com>
2020-08-24 11:51:25 -0300

    Adicionado a funcao de adicionar Todos

--- js/todos.js
+++ js/todos.js
@@ -1,21 +1,34 @@
 var listTodos= document.querySelector('.list ul')
 var form_list= document.querySelector('.form-list input')
-var button_todos= document.querySelector('.form_list button')
+var button_todos= document.querySelector('.form-list button')
 
 var todos= [
-    "Fazer café",
-    "Ligar para o pai",
-    "Fazer lista de compras"
+   "Fazer café",
+   "Ligar para o pai",
+   "Fazer lista de compras"
 ]
 
 function renderTodo(){
-    for(todo of todos){
-        var todoElement= document.createElement('li')
-        var todoText= document.createTextNode(todo)
+   listTodos.innerHTML=''
 
-        todoElement.appendChild(todoText)
-        listTodos.appendChild(todoElement)
-    }
+   for(todo of todos){
+      var todoElement= document.createElement('li')
+      var todoText= document.createTextNode(todo)
+
+      todoElement.appendChild(todoText)
+      listTodos.appendChild(todoElement)
+   }
 }
 
 renderTodo()
+
+function addTodo(){
+   var todosText= form_list.value
+
+   todos.push(todosText)
+   
+   form_list.value=''
+   renderTodo()
+}
+
+button_todos.onclick= addTodo
